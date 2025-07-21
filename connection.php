be3d0dbcabfcd68c3d34344727baa8623619bf78
Luis Sámano <utm21040102@utma.edu.mx>
2025-07-21 13:53:41 -0600

    Cambio de root a admin para conexión exitosa

--- connection.php
+++ connection.php
@@ -1,12 +1,13 @@
 <?php
 $host = 'mysql.railway.internal';
-$user = 'root';
-$password = 'sJmqBEkdSNzjfzxwxuGwIwjluJmTdSsB';
+$user = 'admin';
+$password = 'Admin123!';
 $db = 'railway';
 
 $conn = mysqli_connect($host, $user, $password, $db);
 
 if (!$conn) {
-    die("Conexión fallida: " . mysqli_connect_error());
+    die("❌ Error de conexión: " . mysqli_connect_error());
 }
+echo "✅ Conectado correctamente a la base de datos.";
 ?>
